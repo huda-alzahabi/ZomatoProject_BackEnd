32891f5803950b3ee1b3e9e89f4b6d891de03444
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-05-27 17:53:12 +0300

    Created the login functionality

---
+++ login.php
@@ -0,0 +1,28 @@
+<?php
+
+include("connection.php");
+
+$email = $_POST["login_email"];
+$password = hash("sha256", $_POST["login_pass"]);
+
+$query = $mysqli->prepare("Select id from users where email = ? AND password = ?");
+$query->bind_param("ss", $email, $password);
+$query->execute();
+$query->store_result();
+
+$num_rows = $query->num_rows;
+
+$query->bind_result($id);
+$query->fetch();
+
+$response = [];
+if($num_rows == 0){
+    $response["response"] = "User Not Found";
+}else{
+    $response["response"] = "Logged in";
+    $response["id"] = $id;
+}
+
+echo json_encode($response);
+
+?>
