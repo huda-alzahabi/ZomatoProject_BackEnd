96ec36b747d9178a12c212b598247b3cb5c01093
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-05-31 00:48:14 +0300

    Saved ratings and reviews of customers in db

---
+++ reviews.php
@@ -0,0 +1,28 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+include("connection.php");
+
+$rate = $_POST["rate"];
+$comment = $_POST["comment"];
+$resto_id=1;
+$cust_id=1;
+$rating_status=0;
+
+// $query1 = $mysqli->prepare("Select id from restaurants where email = ? AND password = ?");
+// $query1->bind_param("ss", $email, $password);
+// $query1->execute();
+// $query1->store_result();
+
+// $num_rows = $query1->num_rows;
+
+// $query1->bind_result($resto_id);
+$query = $mysqli->prepare("INSERT INTO ratings(stars, reviews, restaurant_id, customer_id,rating_status) VALUES (?, ?, ?, ?,?)");
+$query->bind_param("isiii", $rate, $comment, $resto_id, $cust_id,$rating_status);
+$query->execute();
+
+$response = [];
+$response["success"] = true;
+
+echo json_encode($response);
+
+?>
