7d0976e78cc3cf0e166705ac5449bd6814fa1dfb
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-06-01 23:15:22 +0300

    Added restaurant id when viewing reviews

--- view_reviews.php
+++ view_reviews.php
@@ -1,6 +1,6 @@
-<?php 
+<?php
 include("connection.php");
-$query = $mysqli->prepare("SELECT id,stars,reviews FROM ratings;");
+$query = $mysqli->prepare("SELECT id,stars,reviews,restaurant_id FROM ratings;");
 $query->execute();
 $array = $query ->get_result();
 $response = [];
